24f0d2c5bc38fb6a1bec5bba7b62708ecd3f8d32
Rahul <rahulsm.1503@gmail.com>
2022-07-31 10:55:52 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '205fc5ab4emshd273516e119716fp1300c3jsndb1b2920f0da',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL =`https://wft-geo-db.p.rapidapi.com/v1/geo`;
+
+export const WEATHER_API_URL=`http://api.openweathermap.org/data/2.5`;
+export const API_KEY="fdd02311150ee97d85076b601ac3a088";
